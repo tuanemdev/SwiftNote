1194d23376c5f9e9d588dd15a4f661b9a5e56f39
Nguyễn Tuấn Anh <nguyentuananh.developer@gmail.com>
2022-11-26 10:49:17 +0700

    Initial Commit

---
+++ SwiftNote/SwiftNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNoteApp.swift
+//  SwiftNote
+//
+//  Created by Nguyen Tuan Anh on 26/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
