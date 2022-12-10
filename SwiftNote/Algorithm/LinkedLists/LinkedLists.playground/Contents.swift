a12b676d81b2baa23d1332a59544a9efd0f5354b
tuanemdev <104725942+tuanemdev@users.noreply.github.com>
2022-12-10 15:57:29 +0700

    LinkedLists

--- SwiftNote/Algorithm/LinkedLists/LinkedLists.playground/Contents.swift
+++ SwiftNote/Algorithm/LinkedLists/LinkedLists.playground/Contents.swift
@@ -6,3 +6,204 @@ import Foundation
  + Value: Giá trị mà bản thân nó lưu trữ
  + Reference: tham chiếu đến vị trí của node tiếp theo sau nó, tham chiếu bằng nil thì nó là phần tử cuối cùng
  */
+
+public class Node<Value> {
+    public var value: Value
+    public var next: Node?
+    public init(value: Value, next: Node? = nil) {
+        self.value = value
+        self.next = next
+    }
+}
+
+extension Node: CustomStringConvertible {
+    public var description: String {
+        guard let next = next else {
+            return "\(value)"
+        }
+        return "\(value) -> " + String(describing: next) + " "
+    }
+}
+
+public struct LinkedList<Value> {
+    public var head: Node<Value>?
+    public var tail: Node<Value>?
+    public init() {}
+    public var isEmpty: Bool {
+        head == nil
+    }
+    
+    // MARK: - Thêm giá trị vào list
+    // O(1)
+    public mutating func push(_ value: Value) {
+        copyNodes()
+        head = Node(value: value, next: head)
+        if tail == nil {
+            tail = head
+        }
+    }
+    
+    // O(1)
+    public mutating func append(_ value: Value) {
+        copyNodes()
+        guard !isEmpty else {
+            push(value)
+            return
+        }
+        
+        tail!.next = Node(value: value)
+        tail = tail!.next
+    }
+    
+    // O(1)
+    @discardableResult
+    public mutating func insert(_ value: Value, after node: Node<Value>) -> Node<Value> {
+        copyNodes()
+        guard tail !== node else {
+            append(value)
+            return tail!
+        }
+        node.next = Node(value: value, next: node.next)
+        return node.next!
+    }
+    
+    // O(i): i bằng index
+    public func node(at index: Int) -> Node<Value>? {
+        var currentNode = head
+        var currentIndex = 0
+        while currentNode != nil && currentIndex < index {
+            currentNode = currentNode!.next
+            currentIndex += 1
+        }
+        return currentNode
+    }
+    
+    // MARK: - Gỡ bỏ giá trị ra khỏi list
+    // O(1)
+    @discardableResult
+    public mutating func pop() -> Value? {
+        copyNodes()
+        defer {
+            head = head?.next
+            if isEmpty { tail = nil }
+        }
+        return head?.value
+    }
+    
+    // O(1)
+    @discardableResult
+    public mutating func removeLast() -> Value? {
+        copyNodes()
+        guard let head = head else { return nil }
+        guard head.next != nil else { return pop() }
+        
+        var prev = head
+        var current = head
+        while let next = current.next {
+            prev = current
+            current = next
+        }
+        prev.next = nil
+        tail = prev
+        return current.value
+    }
+    
+    // O(1)
+    @discardableResult
+    public mutating func remove(after node: Node<Value>) -> Value? {
+        guard let node = copyNodes(returningCopyOf: node) else { return nil }
+        defer {
+            if node.next === tail {
+                tail = node
+            }
+            node.next = node.next?.next
+        }
+        return node.next?.value
+    }
+}
+
+extension LinkedList: CustomStringConvertible {
+    public var description: String {
+        guard let head = head else {
+            return "Empty list"
+        }
+        return String(describing: head)
+    }
+}
+
+extension LinkedList: Collection {
+    public struct Index: Comparable {
+        public var node: Node<Value>?
+        public static func == (lhs: Index, rhs: Index) -> Bool {
+            switch (lhs.node, rhs.node) {
+            case let (left?, right?):
+                return left.next === right.next
+            case (nil, nil):
+                return true
+            default:
+                return false
+            }
+        }
+        public static func < (lhs: Index, rhs: Index) -> Bool {
+            guard lhs != rhs else {
+                return false
+            }
+            let nodes = sequence(first: lhs.node) { $0?.next }
+            return nodes.contains { $0 === rhs.node }
+        }
+    }
+    
+    public var startIndex: Index {
+        Index(node: head)
+    }
+    
+    public var endIndex: Index {
+        Index(node: tail?.next)
+    }
+    
+    public func index(after i: Index) -> Index {
+        Index(node: i.node?.next)
+    }
+    
+    public subscript(position: Index) -> Value {
+        position.node!.value
+    }
+}
+// list[list.startIndex]
+// Array(list.prefix(3))
+// list.reduce(0, +)
+
+// MARK: - Làm LinkedList theo cơ chế Value Type và COW (copy-on-write)
+extension LinkedList {
+    private mutating func copyNodes() {
+        guard !isKnownUniquelyReferenced(&head) else { return }
+        guard var oldNode = head else { return }
+        head = Node(value: oldNode.value)
+        var newNode = head
+        while let nextOldNode = oldNode.next {
+            newNode!.next = Node(value: nextOldNode.value)
+            newNode = newNode!.next
+            oldNode = nextOldNode
+        }
+        tail = newNode
+    }
+    
+    private mutating func copyNodes(returningCopyOf node: Node<Value>?) -> Node<Value>? {
+        guard !isKnownUniquelyReferenced(&head) else { return nil }
+        guard var oldNode = head else {
+            return nil
+        }
+        head = Node(value: oldNode.value)
+        var newNode = head
+        var nodeCopy: Node<Value>?
+        while let nextOldNode = oldNode.next {
+            if oldNode === node {
+                nodeCopy = newNode
+            }
+            newNode!.next = Node(value: nextOldNode.value)
+            newNode = newNode!.next
+            oldNode = nextOldNode
+        }
+        return nodeCopy
+    }
+}
