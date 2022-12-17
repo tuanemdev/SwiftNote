72fa65e91af52ed04d452676388b81f88e4a7c40
tuanemdev <104725942+tuanemdev@users.noreply.github.com>
2022-12-17 15:54:36 +0700

    add FloatingPoint

--- SwiftNote/Swift/Extension/BoolExt.swift
+++ SwiftNote/Swift/Extension/BoolExt.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+extension Bool {
+    var int: Int { self ? 1 : 0 }
+    var string: String { self ? "true" : "false" }
+}
---
+++ SwiftNote/Swift/Extension/FloatingPointExt.swift
@@ -0,0 +1,13 @@
+//
+//  FloatingPointExt.swift
+//  SwiftNote
+//
+//  Created by Nguyen Tuan Anh on 17/12/2022.
+//
+
+import Foundation
+
+extension FloatingPoint {
+    var degreesToRadians: Self { Self.pi * self / Self(180) }
+    var radiansToDegrees: Self { self * Self(180) / Self.pi }
+}
