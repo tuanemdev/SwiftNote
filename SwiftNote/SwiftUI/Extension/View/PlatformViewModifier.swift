8968ea953f246f11da7c7ee3bb5fc96a65d4f9a3
tuanemdev <104725942+tuanemdev@users.noreply.github.com>
2022-12-17 14:33:34 +0700

    add platform modifier

---
+++ SwiftNote/SwiftUI/Extension/View/PlatformViewModifier.swift
@@ -0,0 +1,42 @@
+//
+//  PlatformViewModifier.swift
+//  SwiftNote
+//
+//  Created by Nguyen Tuan Anh on 17/12/2022.
+//
+
+import SwiftUI
+
+extension View {
+    func iOS<Content: View>(_ modifier: (Self) -> Content) -> some View {
+        #if os(iOS)
+        return modifier(self)
+        #else
+        return self
+        #endif
+    }
+    
+    func macOS<Content: View>(_ modifier: (Self) -> Content) -> some View {
+        #if os(macOS)
+        return modifier(self)
+        #else
+        return self
+        #endif
+    }
+    
+    func tvOS<Content: View>(_ modifier: (Self) -> Content) -> some View {
+        #if os(tvOS)
+        return modifier(self)
+        #else
+        return self
+        #endif
+    }
+    
+    func watchOS<Content: View>(_ modifier: (Self) -> Content) -> some View {
+        #if os(watchOS)
+        return modifier(self)
+        #else
+        return self
+        #endif
+    }
+}
