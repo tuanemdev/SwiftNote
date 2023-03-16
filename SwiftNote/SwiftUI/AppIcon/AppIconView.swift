e4a56c41634f99276862fe2e387461435934fe04
tuanemdev <104725942+tuanemdev@users.noreply.github.com>
2023-03-16 23:14:20 +0700

    Change App Icon

---
+++ SwiftNote/SwiftUI/AppIcon/AppIconView.swift
@@ -0,0 +1,126 @@
+//
+//  AppIconView.swift
+//  SwiftNote
+//
+//  Created by Nguyen Tuan Anh on 16/03/2023.
+//
+
+import SwiftUI
+
+struct AppIconView: View {
+    @StateObject var viewModel = ChangeAppIconViewModel()
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                VStack(spacing: 11) {
+                    ForEach(ChangeAppIconViewModel.AppIcon.allCases) { appIcon in
+                        HStack(spacing: 16) {
+                            Image(uiImage: appIcon.preview)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 60, height: 60)
+                                .cornerRadius(12)
+                            Text(appIcon.description)
+                            Spacer()
+                        }
+                        .padding(EdgeInsets(top: 14, leading: 16, bottom: 14, trailing: 16))
+                        .background(viewModel.selectedAppIcon == appIcon ? Color.green.opacity(0.6) : Color.gray)
+                        .cornerRadius(20)
+                        .onTapGesture {
+                            withAnimation {
+                                viewModel.updateAppIcon(to: appIcon)
+                            }
+                        }
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.vertical, 40)
+            }
+        }
+        .background(Color(UIColor.darkGray).ignoresSafeArea())
+    }
+}
+
+struct AppIconView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppIconView()
+    }
+}
+
+// MARK: - ViewModel
+final class ChangeAppIconViewModel: ObservableObject {
+    
+    @Published private(set) var selectedAppIcon: AppIcon
+    
+    init() {
+        if let iconName = UIApplication.shared.alternateIconName, let appIcon = AppIcon(rawValue: iconName) {
+            selectedAppIcon = appIcon
+        } else {
+            selectedAppIcon = .primary
+        }
+    }
+    
+    func updateAppIcon(to icon: AppIcon) {
+        let previousAppIcon = selectedAppIcon
+        selectedAppIcon = icon
+        
+        Task { @MainActor in
+            guard UIApplication.shared.alternateIconName != icon.iconName else {
+                /// No need to update since we're already using this icon.
+                return
+            }
+            
+            do {
+                try await UIApplication.shared.setAlternateIconName(icon.iconName)
+            } catch {
+                /// We're only logging the error here and not actively handling the app icon failure
+                /// since it's very unlikely to fail.
+                print("Updating icon to \(String(describing: icon.iconName)) failed.")
+                /// Restore previous app icon
+                selectedAppIcon = previousAppIcon
+            }
+        }
+    }
+    
+    // MARK: - AppIcon
+    enum AppIcon: String, CaseIterable, Identifiable {
+        case primary = "AppIcon"
+        case darkMode = "AppIcon-TE"
+        
+        var id: String { rawValue }
+        var iconName: String? {
+            switch self {
+            case .primary:
+                /// `nil` is used to reset the app icon back to its primary icon.
+                return nil
+            default:
+                return rawValue
+            }
+        }
+        
+        var description: String {
+            switch self {
+            case .primary:
+                return "Default"
+            case .darkMode:
+                return "Tuan Em"
+            }
+        }
+        
+        var preview: UIImage {
+            UIImage(named: rawValue) ?? UIImage()
+        }
+    }
+    
+    // MARK: - Settings
+    /*
+     Step 1: Thêm các AppIcon mong muốn và Assets
+     Step 2: Vào Project -> Build Settings -> Asset Catalog Compiler - Options
+        Tại đây có 2 tuỳ chọn để lựa chọn
+        + Thứ nhất: Alternate App Icon Sets
+          (Khuyên dùng) Khai báo thủ công các AppIcon sẽ được sử dụng
+        + Thứ hai: Include All App Icon Assets
+          Tự động thêm tất cả các AppIcon vào project
+     */
+}
