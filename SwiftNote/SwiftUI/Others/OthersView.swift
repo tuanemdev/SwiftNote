dfcc5041ebcdd1b1d6a5f450173cd80e872018cd
tuanemdev <104725942+tuanemdev@users.noreply.github.com>
2023-03-18 02:10:05 +0700

    String pluralization

---
+++ SwiftNote/SwiftUI/Others/OthersView.swift
@@ -0,0 +1,20 @@
+//
+//  OthersView.swift
+//  SwiftNote
+//
+//  Created by Nguyen Tuan Anh on 18/03/2023.
+//
+
+import SwiftUI
+
+struct OthersView: View {
+    var body: some View {
+        Text("^[\(3) Person](inflect: true)")
+    }
+}
+
+struct OthersView_Previews: PreviewProvider {
+    static var previews: some View {
+        OthersView()
+    }
+}
