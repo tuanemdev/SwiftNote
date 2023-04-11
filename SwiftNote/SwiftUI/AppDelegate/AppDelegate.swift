82ebf3990ce3772e1dd2fc3644a6528e7183f1d0
tuanemdev <104725942+tuanemdev@users.noreply.github.com>
2023-04-12 01:41:33 +0700

    Update cách landscape bằng cách sử dụng biến singleton

--- SwiftNote/SwiftUI/AppDelegate/AppDelegate.swift
+++ SwiftNote/SwiftUI/AppDelegate/AppDelegate.swift
@@ -8,9 +8,13 @@
 import SwiftUI
 
 class AppDelegate: NSObject, UIApplicationDelegate, ObservableObject {
+    static let shared = UIApplication.shared.delegate as! AppDelegate
+    
     let notificationDelegate: NotificationDelegate = .init()
     
+    // var orientation: UIInterfaceOrientationMask = .allButUpsideDown
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        // Cách 1:
         guard let topViewController = application.topViewController() else { return .allButUpsideDown }
         switch topViewController {
         case is ExampleViewController:
@@ -18,6 +22,11 @@ class AppDelegate: NSObject, UIApplicationDelegate, ObservableObject {
         default:
             return .allButUpsideDown
         }
+        // Cách 2: Sử dụng: AppDelegate.shared.orientation = .landscape (nhớ set lại giá trị mặc định khi dismiss View)
+        // Bổ sung (nếu cần): UIDevice.current.setValue(orientation.rawValue, forKey: "orientation") (chưa rõ lý do hoạt động không ổn định)
+        /*
+         return .orientation
+         */
     }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
