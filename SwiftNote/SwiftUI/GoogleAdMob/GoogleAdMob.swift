a228fbf8bf907d239e4bab1e46c64705d7df9b83
tuanemdev <104725942+tuanemdev@users.noreply.github.com>
2023-04-30 21:08:22 +0700

    google admod

---
+++ SwiftNote/SwiftUI/GoogleAdMob/GoogleAdMob.swift
@@ -0,0 +1,13 @@
+//
+//  GoogleAdMob.swift
+//  SwiftNote
+//
+//  Created by Nguyen Tuan Anh on 30/04/2023.
+//
+
+import SwiftUI
+
+// https://developers.google.com/admob/ios/quick-start?hl=vi
+// https://github.com/googleads/swift-package-manager-google-mobile-ads.git
+// https://developers.google.com/admob/ios/swiftui
+// https://github.com/googleads/googleads-mobile-ios-examples/tree/main/Swift/advanced/SwiftUIDemo
